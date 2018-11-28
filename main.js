398935ceef0749cf6d75a2b300f4e1fd1afb052c
Karolina Charlinska <karolina.charlinska@gmail.com>
2018-11-28 17:51:39 +0100

    add BEM methodology

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const input = document.querySelector('.todo-input');
-const toDoSection = document.querySelector('.todo-section');
-const doneSection = document.querySelector('.done-section');
+const input = document.querySelector('.todo__input');
+const toDoSection = document.querySelector('.todo__section');
+const doneSection = document.querySelector('.done__section');
 
 let active = true;
 
 const currentDate = new Date().toDateString();
 // const day = currentDate.get
 // TODO: change date format
-document.querySelector('.main-section p').textContent = currentDate;
+document.querySelector('.main__section p').textContent = currentDate;
 
 input.addEventListener('keypress', function (e) {
     if (e.keyCode === 13) {
@@ -19,28 +19,28 @@ const addToDo = () => {
     const txt = input.value;
     if (txt === '') return;
     const toDoItem = document.createElement('div');
-    toDoItem.className = 'todo-container';
-    toDoItem.innerHTML = `<div class="todo-dot"><i class="far fa-circle"></i></div>
-    <div class="todo-text">${txt}</div>
-    <div class="todo-icons"><i class="fas fa-pencil-alt"></i><i class="far fa-trash-alt"></i>
+    toDoItem.className = 'todo__container';
+    toDoItem.innerHTML = `<div class="todo__dot"><i class="far fa-circle"></i></div>
+    <div class="todo__text">${txt}</div>
+    <div class="todo__icons"><i class="fas fa-pencil-alt"></i><i class="far fa-trash-alt"></i>
     </div>`;
     const toDoText = toDoItem.children[1];
     toDoSection.appendChild(toDoItem);
     if (toDoItem.clientHeight > input.clientHeight) {
-        toDoText.classList.add('todo-text--long');
+        toDoText.classList.add('todo__text--long');
     }
     // FIXME: check height again after resizing the window and change 'todo-text--long' class
     input.value = '';
     toDoItem.querySelector('.fa-trash-alt').addEventListener('click', deleteToDo);
     toDoItem.querySelector('.fa-pencil-alt').addEventListener('click', editToDo);
-    toDoItem.querySelector('.todo-dot .fa-circle').addEventListener('click', doneToDo);
+    toDoItem.querySelector('.todo__dot .fa-circle').addEventListener('click', doneToDo);
 }
 
 const doneToDo = (e) => {
     const toDoItem = e.target.parentNode.parentNode;
-    toDoItem.classList.contains('todo-container--checked') ? active = false : active = true;
+    toDoItem.classList.contains('todo__container--checked') ? active = false : active = true;
     active ? doneSection.appendChild(toDoItem) : toDoSection.appendChild(toDoItem);
-    toDoItem.classList.toggle('todo-container--checked');
+    toDoItem.classList.toggle('todo__container--checked');
 }
 
 const deleteToDo = (e) => {
@@ -52,7 +52,7 @@ const editToDo = (e) => {
     toDoItem.contentEditable = true;
     toDoItem.focus();
     toDoItem.onkeyup = function () {
-        toDoItem.clientHeight > input.clientHeight ? toDoItem.classList.add('todo-text--long') : toDoItem.classList.remove('todo-text--long');
+        toDoItem.clientHeight > input.clientHeight ? toDoItem.classList.add('todo__text--long') : toDoItem.classList.remove('todo__text--long');
     }
     // FIXME: remove contentEditable after click outside todo
     // const isClickInside = toDoItem.parentNode.contains(e.target);
@@ -61,4 +61,4 @@ const editToDo = (e) => {
     // }
 }
 
-document.querySelector('.add-button').addEventListener('click', addToDo)
+document.querySelector('.add__button').addEventListener('click', addToDo)
